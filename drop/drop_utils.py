96f55956061d92763abeb913cc6316006cd2b8e0
Nitish Gupta <gnnitish@gmail.com>
2019-10-28 15:06:16 -0400

    fixed bug

--- drop/drop_utils.py
+++ drop/drop_utils.py
@@ -962,7 +962,9 @@ def write_predictions(all_examples, all_features, all_results, answering_abiliti
         assert len(nbest_json) >= 1 and len(predicted_answers) >= 1
         if example.answer_annotations:
             drop_metrics(predicted_answers, example.answer_annotations)
-            em, f1 = drop_em_and_f1(predicted=predicted_answers, gold=example.answer_annotations)
+            ground_truth_answer_strings = [answer_json_to_strings(annotation)[0]
+                                           for annotation in example.answer_annotations]
+            em, f1 = drop_em_and_f1(predicted=predicted_answers, gold=ground_truth_answer_strings)
             for output in nbest_json:
                 output["f1"] = f1
                 output["em"] = em
